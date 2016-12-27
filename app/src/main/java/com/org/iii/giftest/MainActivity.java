137a72ddf61cfb31193fddff9608cdc007c10a21
apple <livevil404@gmail.com>
2016-12-28 00:55:02 +0800

    2016-12-28-basic

--- app/src/main/java/com/org/iii/giftest/MainActivity.java
+++ app/src/main/java/com/org/iii/giftest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.org.iii.giftest;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // gif 畫面設定
         mGifLoadingView = new GifLoadingView();
         findViewById(R.id.button).setOnClickListener(
                 new View.OnClickListener() {
@@ -25,10 +28,29 @@ public class MainActivity extends AppCompatActivity {
                         mGifLoadingView.setImageResource(R.drawable.load);
                         mGifLoadingView.show(getFragmentManager(),"");
 
+
+                        //間隔時間5秒跳頁
+
+                        new Handler().postDelayed(new Runnable() {
+                           @Override
+                            public void run() {
+                                final Intent mainIntent = new Intent(MainActivity.this, QRcode.class);
+                                startActivity(mainIntent);
+                                finish();        // 當跳到另一 Activity 時，讓 MainActivity 結束。
+                                // 這樣可以避免使用者按 back 後，又回到該 activity。
+                            }
+                        }, 5000);
                     }
+
                 });
     }
 }
+
+
+
+
+
+
 //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/org/iii/giftest/QRcode.java
@@ -0,0 +1,16 @@
+package com.org.iii.giftest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by apple on 2016/12/27.
+ */
+
+public class QRcode extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qrcode);
+    }
+}
